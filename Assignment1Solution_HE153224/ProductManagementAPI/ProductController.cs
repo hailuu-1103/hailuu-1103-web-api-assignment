d0d4f746a3057ad92ba156cd730c6493e6f5145a
hailuu <hoanghai.luu.71@gmail.com>
2023-02-10 20:36:21 +0700

    demo lab 1

---
+++ Assignment1Solution_HE153224/ProductManagementAPI/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using BusinessObjects;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Abstractions;
+using Repositories;
+
+namespace ProductManagementAPI
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository repository = new ProductRepository();
+
+        //Get: api/products
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetProducts() => this.repository.GetProducts();
+
+        //Post: ProductController/Products
+        [HttpPost]
+        public IActionResult PostProduct(Product product)
+        {
+            this.repository.SaveProduct(product);
+            return this.NoContent();
+        }
+
+        //Get: ProductsController/Delete/5
+        [HttpDelete("id")]
+        public IActionResult DeleteProduct(int id)
+        {
+            var p = this.repository.GetProductById(id);
+            if(p == null) return this.NotFound();
+            this.repository.DeleteProduct(p);
+            return this.NoContent() ;
+        }
+
+        [HttpPut("id")]
+        public IActionResult UpdateProduct(int id, Product product)
+        {
+           var pTmp = this.repository.GetProductById(id);
+            if(pTmp == null) return this.NotFound();
+            this.repository.UpdateProduct(product);
+            return this.NoContent();
+        }
+    }
+}
---
+++ Assignment1Solution_HE153224/ProductManagementWebClient/ProductController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductManagementWebClient
+{
+    using System.Net.Http.Headers;
+    using System.Text.Json;
+    using BusinessObjects;
+
+    public class ProductController : Controller
+    {
+        private readonly HttpClient client = null;
+        private string productApiUrl = "";
+
+        public ProductController()
+        {
+            this.client = new HttpClient();
+            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            this.client.DefaultRequestHeaders.Accept.Add(contentType);
+            this.productApiUrl = "http://localhost:53633/api/products";
+        }
+        public async Task<IActionResult> Index()
+        {
+            var    responseMessage = await this.client.GetAsync(this.productApiUrl);
+            var strData         = await responseMessage.Content.ReadAsStringAsync();
+            
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            var listProducts = JsonSerializer.Deserialize<List<Product>>(strData, options);
+            return this.View(listProducts);
+        }
+
+        //Get: ProductController/Details/5
+        public ActionResult Details(int id)
+        {
+            return this.NoContent();
+        }
+
+        //Get: ProductController/Create
+        public ActionResult Create()
+        {
+            return this.NoContent();
+        }
+        
+        //Post: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product p)
+        {
+            return this.NoContent();
+        }
+        
+        //Get: ProductController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return this.NoContent();
+        }
+        
+        //Post: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            return this.NoContent();
+        }
+        
+        //Get: ProductController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return this.NoContent();
+        }
+        
+        //Post: ProductController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            return this.NoContent();
+        }
+    }
+}
